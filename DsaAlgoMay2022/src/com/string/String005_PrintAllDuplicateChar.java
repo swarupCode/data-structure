f85309a72b6883c781660b5d178e61096262c41a
swarupCode <43297655+swarupCode@users.noreply.github.com>
2022-07-26 01:06:11 +0530

    String: Print All the Duplicate Chars - UPDATED

--- DsaAlgoMay2022/src/com/string/String005_PrintAllDuplicateChar.java
+++ DsaAlgoMay2022/src/com/string/String005_PrintAllDuplicateChar.java
@@ -1,6 +1,8 @@
 package com.string;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class String005_PrintAllDuplicateChar {
     static final int NO_OF_CHARS = 126;
@@ -22,6 +24,29 @@ public class String005_PrintAllDuplicateChar {
             if(count[i] > 1) {
                 System.out.println("Character: "+(char)i + " has "+count[i]+ " occurrences.");
             }
+        }
+    }
+
+    static void printDuplicateChars_HashMap(String str)
+    {
+        HashMap<Character, Integer> count = new HashMap<>();
+        /*Store duplicates present
+        in the passed string */
+        for (int i = 0; i < str.length(); i++) {
+            if (!count.containsKey(str.charAt(i)))
+                count.put(str.charAt(i), 1);
+            else
+                count.put(str.charAt(i), count.get(str.charAt(i)) + 1);
+        }
+
+        /*Print duplicates in sorted order*/
+        for (Map.Entry mapElement : count.entrySet()) {
+            char key = (char)mapElement.getKey();
+            int value = ((int)mapElement.getValue());
+
+            if (value > 1)
+                System.out.println(key
+                        + ", count = " + value);
         }
     }
 }
